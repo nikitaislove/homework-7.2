de54f3ddf40bd6559fb2f784756e836c02e94c96
Nikita <nikita.rychaagov@gmail.com>
2021-04-05 04:27:35 +0300

    Добавьте файлы проекта.

---
+++ homework 7.2/homework 7.2.cpp
@@ -0,0 +1,20 @@
+﻿// Задание 2
+
+#include <iostream>
+#define SPAN(x, y, z) (z > x && z < y ? cout << "True "<< z << " ONO VHODIT OT "<< x << " DO " << y : cout << "False " << z << " ONO NE VHODIT OT " << x << " DO " << y)
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout << "VVEDITE DIAPOZOM CHISEL " << "OT: ";
+	cin >> a;
+	cout << "DO: ";
+	cin >> b;
+	cout << "VASHE CHISLO: ";
+	cin >> c;
+	SPAN(a, b, c);
+
+	return 0;
+}
